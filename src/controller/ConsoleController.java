d53cf9b4ff1ed62bb3eaa2182368ea82b7ca9cd6
jack <arseneau.ja@northeastern.edu>
2022-10-31 21:22:37 -0400

    version 0.1

---
+++ src/controller/ConsoleController.java
@@ -0,0 +1,17 @@
+package controller;
+
+/**
+ * This class represents the controller of a simple interactive image processor
+ * that works in the console. This controller offers a simple text interface in which
+ * the user can type instructions to load, manipulate, and save images.
+ *
+ * <p>This controller works with any PPM image to read its pixels and
+ */
+public class ConsoleController implements ImageProcessingController{
+
+
+  @Override
+  public void control() throws IllegalStateException {
+
+  }
+}
---
+++ src/controller/ImageProcessingController.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * An Interface to represent the controller of the image processing application.
+ */
+public interface ImageProcessingController {
+
+  /**
+   * The main method that relinquishes control of the application to the controller.
+   * @throws IllegalStateException if the controller is unable to transmit output
+   */
+  public void control() throws IllegalStateException;
+}
